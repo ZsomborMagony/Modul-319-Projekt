e1bf00c736ad5ea468a24489d97e2112f1c67bbc
diego2000000 <diego.rombaldoni@gmail.com>
2025-11-24 19:06:26 +0100

    made dice function and a bit of structure for the future

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,6 +1,7 @@
+import mechanics.dice;
 
 public class Main {
     public static void main(String[] args) {
-//test
+
     }
 }
---
+++ Project/src/entetys/playerClasses.java
@@ -0,0 +1,7 @@
+package entetys;
+
+public class playerClasses {
+    public
+
+    }
+}
