967ac52702b9d291d969b7be97fd01a949eee1d2
diego2000000 <diego.rombaldoni@gmail.com>
2025-12-01 13:28:53 +0100

    made a Weapon constructor

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,8 +2,15 @@ import mechanics.MakeCharacter;
 
 import java.util.Scanner;
 
+import entetys.Weapons;
+
 public class Main {
     public static void main(String[] args) {
-       MakeCharacter.makeCharakter();
+       // MakeCharacter.makeCharakter();
+
+        System.out.println("Name: "+Weapons.Club.name);
+        System.out.println( "Damage: "+ Weapons.Club.damageRange);
+        System.out.println("Cost: "+Weapons.Club.cost);
+        System.out.println("Weight: "+Weapons.Club.weight);
     }
 }
