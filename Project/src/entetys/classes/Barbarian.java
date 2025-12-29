8db132ce7fffd461bdf82a592b898455325d9dae
Breevon <safal_chittilappilly@proton.me>
2025-12-29 16:02:32 +0100

    Barbarian general and lvl 1 informations summurized

--- Project/src/entetys/classes/Barbarian.java
+++ Project/src/entetys/classes/Barbarian.java
@@ -4,7 +4,53 @@ import mechanics.Dice;
 import entetys.Player;
 
 public class Barbarian {
+    /*
+    Core:-
+    Primary Ability: Strength
+    Hit Die: d12 per level
+    Armor: Light, Medium, Shields
+     */
 
+    /*
+    Starting option:-
 
+    Option A:
+    Greataxe
+    4 Handaxes
+    15 GP
 
+    Option B:
+    75 GP
+     */
+
+    /*
+    Rage only if:-
+    Armor Restriction: No Heavy Armor
+    Uses: Based on level (on table)
+     */
+
+    /*
+    Rage Effects:-
+    Ends on next turn
+    Zero Damage
+    +Rage Attack Damage
+     */
+
+    /*
+    lvl 1:-
+    Abilities: [Rage, Unarmored Defense, Weapon Mastery]
+
+    Rage (2) and Damage= +2
+
+    Unarmored Defense:
+    Armor class = 10 + dexterity + constitution
+    Shields allowed
+
+    Weapons Mastery: Simple and martial melee weapons
+     */
+
+    /*
+    lvl 2:-
+
+     */
 }
