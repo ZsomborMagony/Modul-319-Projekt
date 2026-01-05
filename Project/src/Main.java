2dcce2686d4eaef4912c780b013d74c9f5e61b8f
diego2000000 <diego.rombaldoni@gmail.com>
2026-01-05 15:23:00 +0100

    added leveling up

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         //MakeCharacter.makeCharakter();
         Attack attack =new Attack();
-        attack.fight(2);
+        attack.fight(0);
 
 
     }
