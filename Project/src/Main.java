141e871015b5ba3ebb3d58713213bfd5bf0d1fa8
diego2000000 <diego.rombaldoni@gmail.com>
2026-01-08 14:30:20 +0100

    moved story int its own class

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -5,80 +5,15 @@ import mechanics.Attack;
 import mechanics.Dice;
 import mechanics.Inventory;
 import mechanics.MakeCharacter;
+import story.Story;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //MakeCharacter.makeCharakter();
-        Inventory inventory=new Inventory();
-       inventory.openInventory();
 
-        System.out.println("King: \n" +
-                "Well, well, well, look what we got here. The almighty hero of this kingdom reduced to such a state. I am a bit disappointed. I thought you would put up a much tougher fight but well it doesn’t matter.\n");
-        System.out.println("Player: \n" +
-                "Why are you doing this? Why are you cooperating with the mosters? Why do you want to kill me?\n");
-        System.out.println("King: \n" +
-                "As to why I am doing this is because you are a big obstacle to my plans. You know it was such a pain in the ass trapping you here. You should have died long ago by my associate that I sent after you, but it doesn’t matter. You know these monster became quite handy in capturing you. Of course they can’t kill you because of your strength but they are extremely useful because of their numbers and annoying abilities.\n");
-        System.out.println("Player: \n" +
-                "Are you controlling them?\n");
-        System.out.println("King:\n" +
-                "Bravo, Bravo as expected from the great hero, you noticed it. Yes I am controlling them. It took months of preparations to be able to control this many monsters but as I see, it was all worth it.\n");
-        System.out.println("Player:\n" +
-                "What is this plan, that you are willing to kill your own soldiers that came with me. You can clearly see, that the people of this county are suffering from hunger, monster attacks and diseases. Why are you so desperate on killing me when I did nothing but good for the people of this country.\n");
-        System.out.println("King:\n" +
-                "Money and Power. Everything is buyable with money. Everything is achievable with Power. Everything is about money and Power. It doesn’t matter what kind of methods you use to get them, because if you have them than you can do whatever you want.\n");
-        System.out.println("Player:\n" +
-                "YOU ARE DOING ALL THIS JUST FOR MONEY AND POWER?! You are selling the country out, letting people suffer, making everything overpriced so you can get enough money?\n");
-        System.out.println("King: \n" +
-                "ENOUGH! I heard more than needed of your bullshit and now die.\n");
-        System.out.println("(The King takes out a strange black orb and points it towards you)");
-        System.out.println("(The black orb lights up and you feal a strange sensation and realize that you are losing your strength)");
-        System.out.println("Player: \n" +
-                "What is that thing? It is taking my powers and strength away.\n");
-        System.out.println("King:\n" +
-                "I myself have little idea about this object, but all I need to know is that it is extremely powerful and is able to kill you.\n");
-        System.out.println("(You feal extremely weak and you nearly fainted)");
-        System.out.println("King:\n" +
-                "HA!HA!HA!Ha! finally the great hero will die and I can finally do things without worrying about you interfering.\n");
-        System.out.println("Player:\n" +
-                "Old King! Remember I will come to find you even if I am in hell. I will climb up from there to find you! \n");
-        System.out.println("(You cough up a mouthful of blood)");
-        System.out.println("(Before you faint, you hear a loud explosions and multiple people attacking)");
-        System.out.println("(In the end you faint because of exhaustion)");
-        System.out.println("\n");
-        System.out.println("\n");
-        System.out.println("\n");
-        System.out.println("\n");
-
-        System.out.println("Player: *Grunt*");
-        System.out.println("(You wake up feeling weak and slightly lightheaded)");
-        System.out.println("(You look around to see 3 similar faces that you recognize. You also realize that you are in the Adventure Hall)");
-        System.out.println("(You remember, that they are the blurry figure you saw before fainting and your childhood friends)");
-        System.out.println("Cleric Companion: Are you feeling any better?");
-        System.out.println("Player: Yeah! I managed to recover thanks to your quick actions, but I feel like I am missing something.");
-        System.out.println("Barbarian Companion: How should I explain it to you? Dou you remember the black orb that the bastard king was holding?");
-        System.out.println("Player: Yeah.\n" +
-                "Barbarian Companion: That thing took away most of your powers somehow. Luckily we were fast enough to intercept it before it took everything away though I don’t know what power you have remaining.\n");
-        System.out.println("Cleric Companion: Considering the situation at hand, I advise you to retire for the time being and enjoy life as much as possible");
-        System.out.println("Mage Companion(traitor): We understand that it is hard for you to accept it but right now you are nothing else but an ordinary human.");
-        System.out.println("Player: I am happy to hear that you guys are so concerned about me, but I can’t give up now that I saw how the king treats his own people and letting them suffer. I must become strong again to be able to stand against the king.");
-        System.out.println("Barbarian Companion: We thought you would say and we also have an idea on how to make you stronger again, but first of all we are going to check how much power you have left.");
-        System.out.println("(After this short discussion, Your team with you approval conducted several small experiences to see how much power you have remaining)");
-        System.out.println("Cleric Companion: It seems like you still have your ability to use any weapon after you touch them(Weapon Mastery) and also magic.");
-        System.out.println("Mage Companion(traitor): *Frowns*");
-        System.out.println("Cleric Companion: It also seems like the power of Weapon Mastery and magic also became stronger.");
-        System.out.println("Player: That’s good. At least I have something to use against the king and his monsters.");
-        System.out.println("Barbarian Companion: Now that we checked your powers, we need you to become stronger and we have the best method for you.");
-        System.out.println("Cleric Companion: After your fight with the king strange dungeons appeared in the basement of the Adventurer’s Hall. We do not know how it appeared. All we know is that they are ranging from low level dungeons to high level ones and that there are 13 of them.");
-        System.out.println("Barbarian Companion: What really strange is that 3 of them are sealed and there are no methods to get in there at the moment.");
-        System.out.println("Player: It certainly is strange.");
-        System.out.println("Mage Companion(traitor): Right now the goal is for you to become stronger and we will inform you when we find some useful information to these dungeon.");
-        System.out.println("Player: Thank you guys for your help again.");
-        System.out.println("Cleric Companion: No Problem.");
-        System.out.println("(You dress up, take a sword and go to the first dungeon)");
-
+        Story.intro();
 
     }
 }
