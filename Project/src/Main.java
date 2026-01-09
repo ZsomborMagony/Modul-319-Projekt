db11ee339cc5c69b8bebc94b99310defd66faf2d
diego2000000 <diego.rombaldoni@gmail.com>
2026-01-09 13:27:35 +0100

    tried fixing array bug again

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,18 +2,20 @@
 import entetys.Armor;
 import entetys.Player;
 import mechanics.Inventory;
+import story.Story;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Inventory chest = new Inventory();
-        Armor[] armors = {Armor.ringMail};
-        System.out.println(armors[0].name);
-        chest.addWeaponToInventory(1, 3);
-        chest.addWeaponToInventory(0, 1);
-        chest.openChest();
-        Player.PlayerStats.playerInventory.openInventory();
-        Player.PlayerStats.playerInventory.openInventory();
+
+//        Inventory chest = new Inventory();
+//        Armor[] armors = {Armor.ringMail};
+//        System.out.println(armors[0].name);
+//        chest.addWeaponToInventory(1, 3);
+//        chest.addWeaponToInventory(0, 1);
+//        chest.openChest();
+//        Player.PlayerStats.playerInventory.openInventory();
+//        Player.PlayerStats.playerInventory.openInventory();
     }
 }
